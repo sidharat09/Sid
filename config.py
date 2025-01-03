84e408ee660a03d49cf46b9a805c99bb76c1fa52
sidharat09 <sid30573@gmail.com>
2025-01-03 23:54:11 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY 
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7763566676:AAFPUFSFHkdnPppDy5ov6tumAsWuTcgltUU")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Mr_sid_001")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1329951770")
+
+
+
+
+
+
+
+  
